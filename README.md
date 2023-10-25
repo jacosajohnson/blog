d7a52970554a024e04d06173cc75a15a868e8e11
Jacosa <jacoconut12@gmail.com>
2023-10-24 17:39:25 -0700

    Added contact me.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # blog
+
+https://jacosajohnson.github.io/blog/ 
+
--- process_form.php
+++ process_form.php
@@ -1,21 +1,19 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    
-    // Set your email address here
-    $to = "jacosa.johnson@gmail.com";
-    
-    $subject = "Contact Form Submission from $name";
-    
-    $headers = "From: $email" . "\r\n" .
-        "Reply-To: $email" . "\r\n" .
-        "X-Mailer: PHP/" . phpversion();
-    
-    // Send the email
-    mail($to, $subject, $message, $headers);
-    
-    // Redirect to a thank you page or display a confirmation message
-    header("Location: thank_you.html");
-}
+if(isset($_POST['submit'])){
+    $to = "jacosa.johnson@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . "";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
