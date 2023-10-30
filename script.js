3f42ecdc824edd99003a9ec4b189e9937fddedff
Jacosa <jacoconut12@gmail.com>
2023-10-30 16:56:06 -0700

    Finished media query for extra large screens.

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-const titleElement = document.getElementById('animated-title');
-const titlePlaceholder = document.getElementById('title-placeholder');
-const titleText = "Hi, I'm Jacosa Johnson";
-const typeSpeed = 100; // Typing speed in milliseconds
-const deleteSpeed = 50; // Deleting speed in milliseconds
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
 
-function typeAndDelete() {
-  
-  titleElement.textContent = ''; // Clear the title
-  typeTitle(0);
-  titleElement.style.display = 'block';
-  titlePlaceholder.style.display = 'none';
-}
+menuToggle.addEventListener("change", function () {
+    if (menuToggle.checked) {
+        menu.classList.add("menu-visible");
+    } else {
+        menu.classList.remove("menu-visible");
+    }
+});
 
-function typeTitle(index) {
-  if (index < titleText.length) {
-    titleElement.textContent += titleText.charAt(index);
-    setTimeout(() => typeTitle(index + 1), typeSpeed);
-  } else {
-    setTimeout(deleteTitle, 1000); // Wait for 1 second before deleting
-  }
-}
 
-function deleteTitle() {
-  const currentText = titleElement.textContent;
-  if (currentText.length > 0) {
-    titleElement.textContent = currentText.slice(0, -1);
-    setTimeout(deleteTitle, deleteSpeed);
-  } else {
-    setTimeout(typeAndDelete, 1000); // Wait for 1 second before repeating
-  }
-}
-
-typeAndDelete();
